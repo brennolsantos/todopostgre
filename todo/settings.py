90da221c9057fd9a624eeaa716a1ce557c8fb988
Brenno Lima dos Santos <75213610+brennolsantos@users.noreply.github.com>
2021-12-01 09:29:27 -0300

    Change secret key

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rtr#u0x0^kjj^u3+($j1s0_21z0skpe9smu7jh+-2qq4fu&v1b'
+SECRET_KEY = 'my-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
